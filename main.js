916a5e241a5e84140825e3bc8da6421b70857b85
yasmim mendes <mendes.silva.yasmim@escola.pr.gov.br>
2026-07-08 08:24:14 -0300

    vghuhv

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const botoes = document.querySelectorall("button")
 console.log(botoes)
+
+botoes.forEach(function(botao){
+    let curtiu = false
+    botao.addEventlistener("click", botaoClicado)
+
+    function botaoClicado(){
+        console.log("botao Clicado")
+        let texto = botao.querySelector("span")
+        if(curtiu == false) {
+            texto.texContent++;
+        }else{
+            texto.texContent--;
+            curtiu = false
+        }
+    }
+}
